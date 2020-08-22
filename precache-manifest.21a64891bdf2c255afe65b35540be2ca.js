0cd7f46facdf1b70f5ee357a8b1214a0a82ca054
TheTeaCat <joshua.r.osullivan@gmail.com>
2020-08-22 14:07:11 +0000

    Deploying to gh-pages from  @ bb257fc2f8fe9e23a130a8ceb91f6381dbe30e68 🚀

---
+++ precache-manifest.21a64891bdf2c255afe65b35540be2ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3e5a2e199b3e6642fcf",
+    "url": "/albums-by-year/css/app.834ef278.css"
+  },
+  {
+    "revision": "dd877a89b80ad78d684546c067cd48ae",
+    "url": "/albums-by-year/index.html"
+  },
+  {
+    "revision": "d3e5a2e199b3e6642fcf",
+    "url": "/albums-by-year/js/app.277a3641.js"
+  },
+  {
+    "revision": "8bdd5d0a4991eb5036c5",
+    "url": "/albums-by-year/js/chunk-vendors.41b397b8.js"
+  },
+  {
+    "revision": "bb7c22c92845aa3cb315f7ec4a7f2a2c",
+    "url": "/albums-by-year/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/albums-by-year/robots.txt"
+  }
+]);
